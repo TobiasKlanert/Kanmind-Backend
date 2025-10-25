e2e9acfbb26a63648961651af5f3cd878f97f654
TobiasKlanert <tobias.klanert@t-online.de>
2025-10-25 20:56:24 +0200

    feat: PATCH method for single board implemented

--- board_app/api/views.py
+++ board_app/api/views.py
@@ -1,6 +1,12 @@
 from rest_framework import generics, permissions, mixins
-from .serializers import BoardSerializer, BoardDetailSerializer
+from rest_framework.response import Response
+from .serializers import (
+    BoardSerializer,
+    BoardDetailSerializer,
+    UserInlineSerializer,
+)
 from .permissions import IsOwnerOrMember
+from auth_app.models import User
 from ..models import Board
 
 
@@ -15,10 +21,38 @@ class BoardListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save()
 
-class BoardDetailView(generics.RetrieveAPIView, mixins.DestroyModelMixin):
+class BoardDetailView(generics.RetrieveAPIView, mixins.DestroyModelMixin, mixins.UpdateModelMixin):
     queryset = Board.objects.all()
     serializer_class = BoardDetailSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrMember]
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+    
+    def patch(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        data = request.data.copy()
+        members_ids = data.pop("members", None)
+
+        serializer = self.get_serializer(instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if members_ids is not None:
+            if not isinstance(members_ids, (list, tuple)):
+                return Response({"detail": "members must be a list of IDs"}, status=400)
+            users = User.objects.filter(id__in=members_ids)
+            if users.count() != len(set(members_ids)):
+                return Response({"detail": "one or more member IDs are invalid"}, status=400)
+            instance.members.set(users)
+
+        owner_data = UserInlineSerializer(instance.owner).data
+        members_data = UserInlineSerializer(instance.members.all(), many=True).data
+
+        return Response({
+            "id": instance.id,
+            "title": instance.title,
+            "owner_data": owner_data,
+            "members_data": members_data,
+        })
